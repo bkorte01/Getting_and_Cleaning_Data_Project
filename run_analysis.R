211b0a323ec47011189af6d289b6dc16c5f2fc73
bkorte01 <bkorte@wustl.edu>
2023-11-27 15:45:37 -0600

    completed script

--- run_analysis.R
+++ run_analysis.R
@@ -6,16 +6,41 @@
 
 
 
-# Create a new folder called "course_project" and set the working directory to this folder. 
+# Create a new folder called 'course_project' and set the working directory to this folder. 
 
 if (!file.exists("course_project")) {
         dir.create("course_project")
 }
 setwd("./course_project")
 
+
 # Download and unzip the raw data into this new folder.
 
 rawdata <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(rawdata, destfile = "./course_project.zip")
 unzip("course_project.zip")
+
+# Read the tables within the unziped files, assign variables to them, and extract basic information 
+# from them.
+features <- read.table("./UCI HAR Dataset/features.txt")
+activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+
+# Read the table that contains all of the features and create an index for only the features that 
+# include entries with 'mean()' or 'std()'.
+
+features <- read.table("./UCI HAR Dataset/features.txt")
+featureIndex <- grep("std()|mean()", features$V2)
+
+read.table("./UCI HAR Dataset/activity_labels.txt")
+
+read.table("./UCI HAR Dataset/test/subject_test.txt")
+read.table("./UCI HAR Dataset/test/X_test.txt")
+read.table("./UCI HAR Dataset/test/Y_test.txt")
+
+read.table("./UCI HAR Dataset/train/subject_train.txt")
+training_set <- read.table("./UCI HAR Dataset/train/X_train.txt")
+training_labels <- read.table("./UCI HAR Dataset/train/Y_train.txt", col.names = "activityType")
+# row.names = c(1 = "walking", 2 = "walkingUpstairs", 3 = "walkingDownstairs", 4 = "sitting", 5 = "standing", 6 = "laying")
+training <- cbind(training_set, training_labels)
 
